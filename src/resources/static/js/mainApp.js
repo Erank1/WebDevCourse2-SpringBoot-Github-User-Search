6cd07c6d1c36479b47ca867ac7f315a018271267
Eran Reuven <51163707+Erank1@users.noreply.github.com>
2021-10-05 20:55:10 +0300

    Delete src/resources directory

--- src/resources/static/js/mainApp.js
+++
@@ -1,4 +0,0 @@
-/*----Eran Reuven----*/
-/*This is a simple js document to hide after 5 sec alerts.*/
-window.setTimeout("document.getElementById('alert').style.display='none';", 5000);
-window.setTimeout("document.getElementById('alert2').style.display='none';", 5000);
