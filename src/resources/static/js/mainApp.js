2e4aa9c947c0fcbe55bf62bcd4be51803c9ae520
Eran Reuven <51163707+Erank1@users.noreply.github.com>
2021-10-05 20:51:47 +0300

    Add files via upload

---
+++ src/resources/static/js/mainApp.js
@@ -0,0 +1,4 @@
+/*----Eran Reuven----*/
+/*This is a simple js document to hide after 5 sec alerts.*/
+window.setTimeout("document.getElementById('alert').style.display='none';", 5000);
+window.setTimeout("document.getElementById('alert2').style.display='none';", 5000);
